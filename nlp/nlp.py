6299e9aabbcd261055646daf2ac73e8fef06d941
Ranjan <ranjangangadhar7102@gmail.com>
2024-05-01 19:33:34 +0530

    updated stemming

--- nlp/nlp.py
+++ nlp/nlp.py
@@ -1,30 +1,23 @@
-#from TrainNLP import TrainNLP
-#from UseNLP import UseNLP
-import TrainNLP
-import UseNLP
+from TrainNLP import TrainNLP
+from UseNLP import UseNLP
 import os
-import sys
+from nltk.stem import PorterStemmer
+
+# Initialize the Porter stemmer
+porter = PorterStemmer()
 
 model_path = './nlp-model/model-best'
 
+# Function to perform stemming using NLTK's Porter stemmer
+def stem_word(word):
+    return porter.stem(word)
+
 
 def nlp_ner():
-    ipText = sys.argv
-    ipText.pop(0)
-    text = ''
-    for word in ipText:
-        text += word
-        text += ' '
+    ipText = input('enter the Text sentence\n')
     usenlp = UseNLP()
-    text = text.split(".")
-    output = []
-    for line in text :
-        output.append(usenlp.getner(line))
-    # entities_dict = usenlp.getner(ipText)
+    entities_dict = usenlp.getner(ipText)
     print('The obtained named entities from the model are')
-<<<<<<< HEAD
-    print(output)
-=======
     
     # Remove trailing and leading spaces from keys
     cleaned_dict = {key.strip(): value for key, value in entities_dict.items()}
@@ -32,7 +25,16 @@ def nlp_ner():
     # Reverse keys and values
     reversed_dict = {value: key for key, value in cleaned_dict.items()}
     print(reversed_dict)
->>>>>>> e5e01e0dd244a9fd337345b49cf5dbe48849ecc5
+    
+   # Perform stemming on the value associated with the 'ACTION' key
+    action_stem = stem_word(reversed_dict['ACTION'])
+
+    # Update the dictionary with the stemmed form of the verb
+    reversed_dict['ACTION'] = action_stem
+    
+    print('After stemming')
+    print(reversed_dict)
+
 
 if os.path.exists(model_path):
     print('model exists\n')
